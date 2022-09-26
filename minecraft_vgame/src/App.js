9d992f796de2a98c006f6766467ffdae4e025971
RajveerNarang <dhanurrajveer@gmail.com>
2022-09-26 12:28:56 +0530

    ground texture half done

--- minecraft_vgame/src/App.js
+++ minecraft_vgame/src/App.js
@@ -1,12 +1,17 @@
 import { Sky } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-
+import { Physics } from "@react-three/cannon";
+import { Ground } from "./components/Ground";
 function App() {
   return (
     <>
       {/* <div> outside Div</div> */}
       <Canvas>
         <Sky sunPosition={[100, 100, 20]} />
+        <ambientLight intensity={0.5} />
+        <Physics>
+          <Ground />
+        </Physics>
       </Canvas>
     </>
   );
--- minecraft_vgame/src/images/images.js
+++ minecraft_vgame/src/images/images.js
@@ -3,13 +3,4 @@ import grassImg from "./grass.jpg";
 import glassImg from "./glass.png";
 import logImg from "./log.jpg";
 import woodImg from "./wood.png";
-
-const images = {
-  dirtImg,
-  grassImg,
-  glassImg,
-  woodImg,
-  logImg,
-};
-
-export default images;
+export { dirtImg, grassImg, glassImg, woodImg, logImg };
