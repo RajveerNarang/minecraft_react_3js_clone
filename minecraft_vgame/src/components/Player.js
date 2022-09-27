6ecd69fd01553cf8592ff63a19d033f814ba1bf1
RajveerNarang <dhanurrajveer@gmail.com>
2022-09-27 15:20:16 +0530

    Forward and Jump of Player done

--- minecraft_vgame/src/components/Player.js
+++ minecraft_vgame/src/components/Player.js
@@ -4,12 +4,15 @@ import { useEffect, useRef } from "react";
 import { Vector3 } from "three";
 import { useKeyboard } from "../hooks/useKeyboard";
 
+const JUMP_FORCE = 7;
+const SPEED = 7;
 export const Player = () => {
-  const actions = useKeyboard();
-  console.log(
-    "actions",
-    Object.entries(actions).filter(([k, v]) => v)
-  );
+  const { moveForward, moveBackward, moveLeft, moveRight, jump } =
+    useKeyboard();
+  // console.log(
+  //   "actions",
+  //   Object.entries(actions).filter(([k, v]) => v)
+  // );
   const { camera } = useThree();
   const [ref, api] = useSphere(() => ({
     mass: 1,
@@ -31,8 +34,32 @@ export const Player = () => {
     camera.position.copy(
       new Vector3(pos.current[0], pos.current[1], pos.current[2])
     );
+    const direction = new Vector3();
 
-    api.velocity.set(0, 1, 0);
+    const frontVector = new Vector3(
+      0,
+      0,
+      (moveBackward ? 1 : 0) - (moveForward ? 1 : 0)
+    );
+
+    const sideVector = new Vector3(
+      0,
+      0,
+      (moveLeft ? 1 : 0) - (moveRight ? 1 : 0)
+    );
+
+    direction
+      .subVectors(frontVector, sideVector)
+      .normalize()
+      .multiplyScalar(SPEED)
+      .applyEuler(camera.rotation);
+
+    api.velocity.set(direction.x, vel.current[1], direction.z);
+
+    // jump action
+    if (jump && Math.abs(vel.current[1]) < 0.05) {
+      api.velocity.set(vel.current[0], JUMP_FORCE, vel.current[2]);
+    }
   });
   return <mesh ref={ref}></mesh>;
 };
