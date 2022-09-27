555b5f3582593c6f8dcbecd3f49af78c42b9f916
RajveerNarang <dhanurrajveer@gmail.com>
2022-09-28 00:33:04 +0530

    completed

--- minecraft_vgame/src/hooks/useStore.js
+++ minecraft_vgame/src/hooks/useStore.js
@@ -1,44 +1,46 @@
 import create from "zustand";
 import { nanoid } from "nanoid";
 
+const getLocalStorage = (key) => JSON.parse(window.localStorage.getItem(key));
+const setLocalStorage = (key, value) =>
+  window.localStorage.setItem(key, JSON.stringify(value));
+
 export const useStore = create((set) => ({
   texture: "dirt",
-  cubes: [
-    {
-      key: nanoid(),
-      pos: [2, 0.5, 0],
-      texture: "dirt",
-    },
-
-    {
-      key: nanoid(),
-      pos: [1, 0.5, 0],
-      texture: "wood",
-    },
-
-    {
-      key: nanoid(),
-      pos: [3, 0.5, 0],
-      texture: "glass",
-    },
-    {
-      key: nanoid(),
-      pos: [4, 0.5, 0],
-      texture: "grass",
-    },
-
-    {
-      key: nanoid(),
-      pos: [5, 0.5, 0],
-      texture: "log",
-    },
-
-    // {
-    //   key: nanoid(),
-    //   pos: [6, 0.5, 0],
-    //   texture: "ground",
-    // },
-  ],
+  cubes:
+    getLocalStorage("cubes") ||
+    [
+      // {
+      //   key: nanoid(),
+      //   pos: [2, 0.5, 0],
+      //   texture: "dirt",
+      // },
+      // {
+      //   key: nanoid(),
+      //   pos: [1, 0.5, 0],
+      //   texture: "wood",
+      // },
+      // {
+      //   key: nanoid(),
+      //   pos: [3, 0.5, 0],
+      //   texture: "glass",
+      // },
+      // {
+      //   key: nanoid(),
+      //   pos: [4, 0.5, 0],
+      //   texture: "grass",
+      // },
+      // {
+      //   key: nanoid(),
+      //   pos: [5, 0.5, 0],
+      //   texture: "log",
+      // },
+      // {
+      //   key: nanoid(),
+      //   pos: [6, 0.5, 0],
+      //   texture: "ground",
+      // },
+    ],
   addCube: (x, y, z) => {
     set((prev) => ({
       cubes: [
@@ -47,7 +49,28 @@ export const useStore = create((set) => ({
       ],
     }));
   },
-  setTexture: () => {},
-  saveWorld: () => {},
-  resetWorld: () => {},
+
+  removeCube: (x, y, z) => {
+    set((prev) => ({
+      cubes: prev.cubes.filter((cube) => {
+        const [X, Y, Z] = cube.pos;
+        return X !== x || Y !== y || Z !== z;
+      }),
+    }));
+  },
+  setTexture: (texture) => {
+    set(() => ({
+      texture,
+    }));
+  },
+  saveWorld: () => {
+    set((prev) => {
+      setLocalStorage("cubes", prev.cubes);
+    });
+  },
+  resetWorld: () => {
+    set(() => ({
+      cubes: [],
+    }));
+  },
 }));
